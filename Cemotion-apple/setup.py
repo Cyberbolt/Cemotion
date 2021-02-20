a96f1a758d6529b50c326c76c81ab8e7bea31d43
DTC@Cyberbolt <DTC@Cyberbolt.(none)>
2021-02-20 19:49:09 +0800

    Cemotion 0.2.0

---
+++ Cemotion-apple/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Cemotion-apple",
+    version="0.2.0",
+    author="Cyberbolt",
+    author_email="735245473@qq.com",
+    description="基于NLP的中文情感倾向分析库",
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'tqdm>=4.56.0',
+        'requests>=2.25.1',
+        'jieba>=0.42.1',
+    ]    
+)
