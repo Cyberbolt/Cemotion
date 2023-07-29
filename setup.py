0c968f0573906fe7b89af252dc4ddfabd1ae62f2
Cyberbolt <735245473@qq.com>
2023-07-29 17:38:58 +0800

    Fix errors caused by incorrect transformers version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Cemotion",
-    version="2.0.3",
+    version="2.0.4",
     author="Cyberbolt",
     author_email="735245473@qq.com",
     description="基于NLP的中文情感倾向分析库",
@@ -24,6 +24,6 @@ setuptools.setup(
         'requests>=2.25.1',
         'numpy>=1.19.5',
         'torch>=2.0.0',
-        'transformers>=4.24.0',
+        'transformers==4.24.0',
     ]    
 )
